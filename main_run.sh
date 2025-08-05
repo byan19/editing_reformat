b3b97401fcc24a16c392309c49596462e0d6481b
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-08-05 16:06:41 +0100

    from mac

---
+++ main_run.sh
@@ -0,0 +1,8 @@
+python3
+-m experiments.evaluate \
+--alg_name=AlphaEdit \
+--model_name=meta-llama/Meta-Llama-3-8B-Instruct \
+--hparams_fname=Llama3-8B.json \
+--ds_name=mcf --dataset_size_limit=2000 \
+--num_edits=100 \
+--downstream_eval_steps=5
