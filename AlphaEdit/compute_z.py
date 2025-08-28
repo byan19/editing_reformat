80979b36b90a20a491c2344217fae5a16e1002cf
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-08-28 17:30:05 +0100

    from mac

--- AlphaEdit/compute_z.py
+++ AlphaEdit/compute_z.py
@@ -419,7 +419,7 @@ def compute_z(
         # precised version
         flat_loss += torch.nn.functional.softplus(
             -1 * noise_holder[0][torch.arange(logits.size(0)), pred_loc] @ (grad_noise - grad).t() / noise_scale)
-        gradient_check = torch.autograd.grad(flat_loss.mean(), [delta], retain_graph = True)
+        gradient_check = torch.autograd.grad(flat_loss.mean(), [delta], retain_graph = True)[0]
         
         print(f' check gradient, {gradient_check.norm().item()}')
         
