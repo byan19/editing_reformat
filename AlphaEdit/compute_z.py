2ff9a197b0eb3c5ece5c57ec75b85ecf334f516f
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-08-28 16:47:18 +0100

    from mac

--- AlphaEdit/compute_z.py
+++ AlphaEdit/compute_z.py
@@ -316,7 +316,8 @@ def compute_z(
             #logits = model(**input_tok).logits
             
             output = model(**input_tok,output_hidden_states=True)
-            hidden_states = output.hidden_states[layer-1: layer+1]
+            #hidden_states = output.hidden_states[layer-1: layer+1]
+            hidden_states = output.hidden_states[layer: layer+2]
             logits = output.logits
 
             # Compute distribution for KL divergence
@@ -371,7 +372,7 @@ def compute_z(
                 hooks.append(model.model.layers[layer].register_forward_pre_hook(hook_fn_local))
                 
                 noise_output = model(**input_tok, output_hidden_states=True)
-                noise_hidden_states = noise_output.hidden_states[layer: layer+1]
+                noise_hidden_states = noise_output.hidden_states[layer: layer+2]
                 
             
 
