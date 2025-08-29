b69df91b9bcddf21e93ae29046da294a16a8018c
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-08-29 14:46:56 +0100

    from mac

--- debugging_test.py
+++ debugging_test.py
@@ -2,17 +2,22 @@ import torch
 from transformers import AutoModel, AutoTokenizer
 import pdb
 
-model = AutoModel.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
+model_name = 'meta-llama/Meta-Llama-3-8B-Instruct'
+model_name = 'meta-llama/Llama-2-7b-hf'
+
+model = AutoModel.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 
 model.to('cuda')
 inputs = tokenizer("Hello world", return_tensors="pt")
 # forward with hidden states
 #outputs = model(**inputs, output_hidden_states=True)
+'''
 for name, param in model.named_parameters():
 	param.requires_grad = False
-	
+'''
+
 outputs = model(inputs['input_ids'].cuda(), inputs['attention_mask'].cuda() , output_hidden_states=True)
 
 hidden = outputs.hidden_states[-1]   # last layer hidden states
