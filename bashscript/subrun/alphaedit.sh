dee90730a69ba653f1d8075bf37563e575bfe9e2
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-09-11 10:45:05 +0100

    from mac

--- bashscript/subrun/alphaedit.sh
+++ bashscript/subrun/alphaedit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=0 python -m experiments.evaluate \
 --alg_name=AlphaEdit \
---model_name=/root/autodl-tmp/model/meta-llama3-8b-instruct \
+--model_name=meta-llama/Meta-Llama-3-8B-Instruct \
 --hparams_fname=Llama3-8B.json \
 --ds_name=mcf \
 --dataset_size_limit=2000 \
@@ -12,5 +12,5 @@ CUDA_VISIBLE_DEVICES=0 python -m experiments.evaluate \
 
 # the default choise of the models
 #--model_name=meta-llama/Llama-2-7b-hf \
-#--model_name=meta-llama/Meta-Llama-3-8B-Instruct \
+#--model_name=/root/autodl-tmp/model/meta-llama3-8b-instruct \
 #--num_edits=100 \
