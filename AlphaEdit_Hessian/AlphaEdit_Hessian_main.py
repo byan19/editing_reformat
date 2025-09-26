b01b4eb4522b17d2b05fbe469c9818dca99d7d5d
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-09-26 10:11:41 +0100

    from mac

--- AlphaEdit_Hessian/AlphaEdit_Hessian_main.py
+++ AlphaEdit_Hessian/AlphaEdit_Hessian_main.py
@@ -153,7 +153,8 @@ def apply_AlphaEdit_Hessian_to_model(
         '''
         
         tmp = upd_matrix @ (fisher_matrix/ fisher_matrix.max() + torch.eye(fisher_matrix.shape[0], dtype=torch.float,device="cuda"))
-        print(upd_matrix)
+        print('the final solution with fisher matrix')
+        print(tmp)
         # Adjust update matrix shape
         weight_name = f"{hparams.rewrite_module_tmp.format(layer)}.weight"
         upd_matrix = upd_matrix_match_shape(upd_matrix, weights[weight_name].shape)
