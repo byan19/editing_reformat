9a3227020fe9dcacb03daecdf16025a082e9b967
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-10-10 15:44:04 +0100

    fisher_matrix no eye

--- bashscript/evaluation.sh
+++ bashscript/evaluation.sh
@@ -6,5 +6,6 @@
 #python -m experiments.summarize --dir_name=AlphaEdit_Hessian --runs=run_011,run_012
 #python -m experiments.summarize --dir_name=AlphaEdit --runs=run_007
 
-python -m experiments.summarize --dir_name=AlphaEdit_Hessian --runs=run_011,run_012
-python -m experiments.summarize --dir_name=AlphaEdit --runs=run_001
+#python -m experiments.summarize --dir_name=AlphaEdit_Hessian --runs=run_011,run_012
+#python -m experiments.summarize --dir_name=AlphaEdit --runs=run_001
+python -m experiments.summarize --dir_name=AlphaEdit_Hessian --runs=run_004
