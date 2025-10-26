e1596ffc9cfe41e7a24db331744d4318944a40c6
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-10-26 13:35:53 +0000

    baseline_noneediting

--- AlphaEdit_Hessian/AlphaEdit_Hessian_main.py
+++ AlphaEdit_Hessian/AlphaEdit_Hessian_main.py
@@ -155,7 +155,7 @@ def apply_AlphaEdit_Hessian_to_model(
         tmp = upd_matrix @ (fisher_matrix/ fisher_matrix.max() + torch.eye(fisher_matrix.shape[0], dtype=torch.float,device="cuda"))
         '''
         
-        if hparams.hessian_type == 'largest_norm':
+        if hparams.hessian_type == 'largest_norm' or hparams.hessian_type == 'soft_largest_norm':
             holder_matrix = upd_matrix.detach().clone()
             
         upd_matrix = upd_matrix @ hessian[i, :, : ].cuda()
